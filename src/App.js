3a0231901abc27d088db3425a6a41fad6bc9b54e
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-08-25 21:54:07 +0200

    Moved gifcards state from app component. And added giftcard methods

--- src/App.js
+++ src/App.js
@@ -2,39 +2,12 @@ import React from 'react';
 import GiftCards from './components/giftcards/Giftcards';
 
 class App extends React.Component {
-  state = {
-    giftcards: [
-      {
-        id: 0,
-        val: 500,
-        sold_on: '19-10-1998',
-        sold_by: 'Nikolai',
-        received_on: '',
-        received_by: 'Nikolai'
-      },
-      {
-        id: 1,
-        val: 900,
-        sold_on: '09-10-1998',
-        sold_by: 'Nikolai',
-        received_on: '20-10-2020',
-        received_by: 'Nikolai'
-      },
-      {
-        id: 2,
-        val: 300,
-        sold_on: '19-10-1998',
-        sold_by: 'Nikolai',
-        received_on: '23-02-1999',
-        received_by: 'Nikolai'
-      }
-    ]
-  }
+
 
   render() {
     return (
       <div className="container">
-        <GiftCards giftcards={this.state.giftcards}/>
+        <GiftCards/>
       </div>
     );
   }
--- src/components/giftcards/Giftcards.js
+++ src/components/giftcards/Giftcards.js
@@ -1,21 +1,74 @@
 import React from 'react';
 import GiftCardItem from './GiftcardItem';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import GiftcardForm from './GiftcardForm';
 
 class GiftCards extends React.Component {
+  state = {
+    giftcards: [
+      {
+        id: 0,
+        card_number: 0,
+        val: 500,
+        sold_on: '2019-01-19',
+        sold_by: 'Nikolai',
+        received_on: '2019-06-23',
+        received_by: 'Nikolai'
+      },
+      {
+        id: 1,
+        card_number: 1,
+        val: 900,
+        sold_on: '',
+        sold_by: 'Nikolai',
+        received_on: '',
+        received_by: 'Nikolai'
+      },
+      {
+        id: 2,
+        card_number: 2,
+        val: 300,
+        sold_on: '',
+        sold_by: 'Nikolai',
+        received_on: '',
+        received_by: 'Nikolai'
+      }
+    ]
+  }
+
+  addGiftcard = (giftcard) => {
+    const giftcards = [...this.state.giftcards];
+    giftcards.push(giftcard);
+
+    this.setState({giftcards: giftcards});
+  }
+
+  editGiftcard = (giftcard) => {
+    const giftcards = [...this.state.giftcards];
+    for (var i = 0; i < giftcards.length; i++) {
+      if (giftcards[i].id === giftcard.id) {
+        giftcards[i] = giftcard;
+        break;
+      }
+    }
+
+
+    this.setState({giftcards: giftcards})
+  }
+
   render() {
     return (
       <div>
         <GiftcardForm
          btnTxt="Add a new giftcard"
          btnIcon={<i className="fa fa-plus-square" aria-hidden="true"></i>}
+         submitFnc={this.addGiftcard}
          modalHdr="Create new giftcard"
         />
         <table className="table">
           <thead>
             <tr>
-              <th>Id</th>
+              <th>Card Number</th>
               <th>Value</th>
               <th>Sold on</th>
               <th>Sold by</th>
@@ -26,8 +79,8 @@ class GiftCards extends React.Component {
           </thead>
           <tbody>
             {
-              this.props.giftcards.map((giftcard) => (
-                <GiftCardItem key={giftcard.id} giftcard={giftcard}/>
+              this.state.giftcards.map((giftcard) => (
+                <GiftCardItem key={giftcard.id} giftcard={giftcard} editFunc={this.editGiftcard}/>
               ))
             }
           </tbody>
@@ -38,7 +91,7 @@ class GiftCards extends React.Component {
 }
 
 GiftCards.propTypes = {
-  giftcards: PropTypes.array.isRequired
+  // giftcards: PropTypes.array.isRequired
 }
 
 export default GiftCards;
