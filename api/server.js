b809630b7648dec0addedf8e4f5bba9980d9dc76
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-09-05 22:17:22 +0200

    Added cors dependency

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(morgan('combined'));
 app.use('/api/giftcards', require('./routes/giftcards.js'));
+
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
