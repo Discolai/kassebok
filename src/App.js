d5e03d311b1022b0e3e6793e1f656ab062d05524
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-09-06 23:05:31 +0200

    Added local css assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import GiftCards from './components/giftcards/Giftcards';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class App extends React.Component {
 
