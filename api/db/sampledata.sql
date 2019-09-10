2670f76da0a0b986ec620fbba7c8e781d9bc4bf2
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-09-10 20:48:41 +0200

    Sample data script

---
+++ api/db/sampledata.sql
@@ -0,0 +1,16 @@
+INSERT INTO GiftCards
+(cardId, value, soldOn, soldBy, receivedOn, receivedBy)
+VALUES
+(100, 500, "2019-03-23", "Nikolai", Null, Null),
+(110, 100, "2019-04-23", "Nikolai", Null, Null),
+(120, 300, "2019-05-02", "Nikolai", Null, Null),
+(130, 600, "2019-06-29", "Nikolai", Null, Null),
+(140, 900, "2019-08-13", "Nikolai", Null, Null);
+
+INSERT INTO TodosTemplates
+(monday, tuesday, wednesday, thursday, friday, saturday, message)
+VALUES
+(1, 1, 1, 1, 1, 1, "Take out the trash"),
+(1, 1, 1, 1, 1, 1, "Clean the toilet"),
+(1, 1, 1, 1, 1, 1, "Vacuum the carpets"),
+(1, 0, 0, 1, 0, 0, "Clean the coffe machine milk container");
