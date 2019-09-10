416101f95dccb64c63fb6824264ffacae35b4ef9
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-09-10 22:09:11 +0200

    Started implementing todos

---
+++ src/components/todos/todoitem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class TodoItem extends React.Component {
+  render () {
+    const {todo} = this.props;
+
+    return (
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => this.props.onChange(todo)}
+        />
+      <label className="form-check-label">
+        {todo.message}
+      </label>
+      </div>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem;
--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,15 +1,47 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import axios from 'axios'
 
 import NavBar from '../navbar';
+import TodoItem from './todoitem';
 
 class Todos extends React.Component {
+  state = {
+    todos: []
+  }
+
+  onClick = (todo) => {
+    todo.completed = !todo.completed;
+    axios.put(`http://localhost:8080/api/todos/${todo.id}`, todo)
+    .then((response) => {
+      const todos = [...this.state.todos].map((t) => t = t.id === todo.id ? todo : t);
+
+      this.setState({todos: todos});
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  };
+
+  componentDidMount() {
+    const date = new Date().toISOString().slice(0,10);
+    axios.get(`http://localhost:8080/api/daily-todos/${date}`).then((response) => {
+      // console.log(response);
+      this.setState({todos: response.data.res});
+    });
+  }
+
   render () {
     return (
       <React.Fragment>
         <NavBar/>
         <div className="container">
           <h1>Todos</h1>
+          {
+            this.state.todos.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} onChange={this.onClick} />
+            ))
+          }
         </div>
       </React.Fragment>
     );
